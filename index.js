c21bac64b31615550ea5f99503d87e4e7281f4ea
Peter Pajer <peter.p.pajer@gmail.com>
2020-09-16 14:17:00 +0200

    fixed baseUrl

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const browser = require('browser.js');
 const parsePage = require('parsePage.js');
 const getLocation = require('getLocation.js');
 
-const baseUrl = 'https://www.google.com/search';
+const baseUrl = 'https://www.google.com/search?';
 
 module.exports = async (query, location = false, pages = 1, mobile = false) => {
-	let options = {};
-	options.q = query;
+	let options = {q: query};
 
 	if (location && getLocation(location)) {
 		options.uule = getLocation(location);
